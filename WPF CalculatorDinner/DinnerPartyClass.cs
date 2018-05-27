5aeddd50f852fc619887c8849d7199778c533ff6
MacDeath667 <rom12.93@bk.ru>
2018-05-27 21:48:38 +0300

    Init commit (exception trouble)

---
+++ WPF CalculatorDinner/DinnerPartyClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_CalculatorDinner
+{
+    class DinnerParty
+    {
+        public int NumberOfPeople;
+        public int CostOfBeveragesPerPerson;
+        public decimal CostOfDecorations;
+        public const int CostOfFoodPerPerson = 25;
+
+
+        public int SetHealthyOption(bool healthy)
+        {
+            if (healthy)
+            {
+                CostOfBeveragesPerPerson = 5 * NumberOfPeople; // cost without alco
+                return CostOfBeveragesPerPerson;
+            }
+            CostOfBeveragesPerPerson = 20 * NumberOfPeople; // cost with alco
+            return CostOfBeveragesPerPerson;
+        }
+
+        public decimal CalculateCostOfDecorations(bool decor)
+        {
+            if (decor)
+            {
+                CostOfDecorations = 15 * NumberOfPeople + 50; // cost with decor
+                return CostOfDecorations;
+            }
+            CostOfDecorations = 7.5m * NumberOfPeople + 30; // cost without decor
+            return CostOfDecorations;
+        }
+
+        public decimal CalculateCost(bool healthy)
+        {
+            decimal cost = CostOfBeveragesPerPerson+CostOfDecorations+CostOfFoodPerPerson*NumberOfPeople;
+            if (healthy)
+                cost *= 0.95m;
+            return cost;
+        }
+    }
+}
---
+++ WPF CalculatorDinner/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_CalculatorDinner;
+
+namespace WPF_CalculatorDinner
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DinnerParty dinnerParty = new DinnerParty();
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            //dinnerParty = new DinnerParty() { NumberOfPeople = 5 };
+            //dinnerParty.SetHealthyOption(false);
+            //dinnerParty.CalculateCostOfDecorations(false);
+            //DisplayDinnerPartyCost();
+            //totalSum.Content = "Sum is here";
+        }
+
+        public void DisplayDinnerPartyCost()
+        {
+            decimal Cost = dinnerParty.CalculateCost(CheckHealth(healthy.IsChecked));
+            //totalSum.Content = Cost.ToString("c");
+            string message = Cost.ToString();
+            totalSum.Content = message;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private bool CheckHealth(bool? isChecked)
+        {
+            if (healthy.IsChecked == true)
+                return true;
+            return false;
+        }
+
+        // заколхозили UpDownNumeric
+        private void cmdDown_Click(object sender, RoutedEventArgs e)
+        {
+            txtNum.Text = (int.Parse(txtNum.Text) - 1).ToString();
+        }
+
+        private void cmdUp_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            txtNum.Text = (int.Parse(txtNum.Text) + 1).ToString();
+        }
+
+        private void txtNum_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            {
+                dinnerParty.NumberOfPeople = int.Parse(txtNum.Text);
+                DisplayDinnerPartyCost();
+            }
+        }
+    }
+
+
+
+
+}
+
+
